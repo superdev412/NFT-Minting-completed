5eefc9093722ed36b9d1168876f5b04030df59bc
superdev412 <superdeveloper4913@gmail.com>
2021-12-10 09:09:47 +0300

    first commit

---
+++ precache-manifest.84d97178de7e9495f6a1779d8e827c67.js
@@ -0,0 +1,554 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebe39f5673895813d30bedfc953805ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32fdd3e79f6e60192c9e",
+    "url": "/static/css/3.a192c751.chunk.css"
+  },
+  {
+    "revision": "fd80604c6e1fdad0a4c1",
+    "url": "/static/css/4.7ba2af66.chunk.css"
+  },
+  {
+    "revision": "06fc07f53a96329a3afa",
+    "url": "/static/css/main.34c54b6e.chunk.css"
+  },
+  {
+    "revision": "2dba43185bb3f9ffc44a",
+    "url": "/static/js/0.2a56a4ad.chunk.js"
+  },
+  {
+    "revision": "4f578d98e9c41a7ffaa5",
+    "url": "/static/js/10.892f62de.chunk.js"
+  },
+  {
+    "revision": "dc31de295e891c56280e",
+    "url": "/static/js/11.4b6618c2.chunk.js"
+  },
+  {
+    "revision": "ed377252c4248762bfa6",
+    "url": "/static/js/12.454516fb.chunk.js"
+  },
+  {
+    "revision": "e33ab002e93f23233e29",
+    "url": "/static/js/13.1e85ead1.chunk.js"
+  },
+  {
+    "revision": "5acb528095fdd1e66034",
+    "url": "/static/js/14.89be82ae.chunk.js"
+  },
+  {
+    "revision": "009a374166546b7448e5",
+    "url": "/static/js/15.77e150fa.chunk.js"
+  },
+  {
+    "revision": "97bce7d6c963adcab5e3",
+    "url": "/static/js/16.cbbc5f89.chunk.js"
+  },
+  {
+    "revision": "6cdc00bf38dfccc1e198",
+    "url": "/static/js/17.1a06ebf1.chunk.js"
+  },
+  {
+    "revision": "fe6490faafc05331c34f",
+    "url": "/static/js/18.f889bf0a.chunk.js"
+  },
+  {
+    "revision": "227b290a91af4341945c",
+    "url": "/static/js/19.6e760221.chunk.js"
+  },
+  {
+    "revision": "f113e45e4eef320c4074",
+    "url": "/static/js/20.fbb26eb8.chunk.js"
+  },
+  {
+    "revision": "f548f9be70b836ce7328",
+    "url": "/static/js/21.924cf25a.chunk.js"
+  },
+  {
+    "revision": "3d5061c22485412ba5a2",
+    "url": "/static/js/22.a1aef3e3.chunk.js"
+  },
+  {
+    "revision": "aaefae12d7650d9e771c",
+    "url": "/static/js/23.48fcbd3e.chunk.js"
+  },
+  {
+    "revision": "2ee781714ec1e7fee41d",
+    "url": "/static/js/24.8572060f.chunk.js"
+  },
+  {
+    "revision": "c4a10cfe0723a33420e1",
+    "url": "/static/js/25.a11faeec.chunk.js"
+  },
+  {
+    "revision": "5f0f100282edfad4c8ef",
+    "url": "/static/js/26.9a0c33db.chunk.js"
+  },
+  {
+    "revision": "95fda1efb05934bca711",
+    "url": "/static/js/27.431d5422.chunk.js"
+  },
+  {
+    "revision": "f70d6e967783f046f710",
+    "url": "/static/js/28.c759fe80.chunk.js"
+  },
+  {
+    "revision": "32fdd3e79f6e60192c9e",
+    "url": "/static/js/3.588054a0.chunk.js"
+  },
+  {
+    "revision": "33e9d719335391ca1fe0264499a194b1",
+    "url": "/static/js/3.588054a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd80604c6e1fdad0a4c1",
+    "url": "/static/js/4.59855020.chunk.js"
+  },
+  {
+    "revision": "d4e741ccd5c70b02f6e8",
+    "url": "/static/js/5.9bf8f189.chunk.js"
+  },
+  {
+    "revision": "fa95222cd908b7e7cf6f",
+    "url": "/static/js/6.21fefa52.chunk.js"
+  },
+  {
+    "revision": "194bd45262e038c4e28a",
+    "url": "/static/js/7.2492a54c.chunk.js"
+  },
+  {
+    "revision": "672cdc7109d6ccb4744b",
+    "url": "/static/js/8.70b3ed05.chunk.js"
+  },
+  {
+    "revision": "ed42b56eaa1654798e64",
+    "url": "/static/js/9.d0f3df14.chunk.js"
+  },
+  {
+    "revision": "06fc07f53a96329a3afa",
+    "url": "/static/js/main.2c015814.chunk.js"
+  },
+  {
+    "revision": "105996092434972c5595",
+    "url": "/static/js/runtime-main.8e1ed8c3.js"
+  },
+  {
+    "revision": "7a749abb8a2d8fdf14bc6c6c60571b40",
+    "url": "/static/media/A80.7a749abb.png"
+  },
+  {
+    "revision": "a075fcc87270be3359b3230e52e9ac5b",
+    "url": "/static/media/A920-1-2_530x@2x.a075fcc8.png"
+  },
+  {
+    "revision": "e2ba44ebae1169b924c42efac055fc2a",
+    "url": "/static/media/Big-commerce.e2ba44eb.jpg"
+  },
+  {
+    "revision": "3b7069cf6bb9e0bd92ea39a33a4b3da1",
+    "url": "/static/media/Bonsai-Express-Stations-SR17-8R1-768x609-1.3b7069cf.jpg"
+  },
+  {
+    "revision": "9aa15e003950fe123d3db887c445f1b2",
+    "url": "/static/media/Bonsai-Isometric-Illustration_SR17-9R2.9aa15e00.jpg"
+  },
+  {
+    "revision": "f74c4dce8312d689eab15f5380be9b4f",
+    "url": "/static/media/Bonsai-On-Phones-Graphic-with-Chart-Lines.f74c4dce.png"
+  },
+  {
+    "revision": "1dc18ad7ba67309302ebb191086b9816",
+    "url": "/static/media/Bonsai-POS-with-Credit-card-machine-SR17-8R1.1dc18ad7.png"
+  },
+  {
+    "revision": "c3654ad1b3a7f4cd1dfef6d940e462f7",
+    "url": "/static/media/Bonsai-Tablet-point-of-sale-restaurant-updated-items-SR17-8R2.c3654ad1.jpg"
+  },
+  {
+    "revision": "d83272f4dbfec90235c678c489008a85",
+    "url": "/static/media/Bonsai-in-restaurant-SR17-8R6.d83272f4.jpg"
+  },
+  {
+    "revision": "d842c3b4bda7bbdffc6058a5ff68efe9",
+    "url": "/static/media/Business-Loans-panorama.d842c3b4.jpg"
+  },
+  {
+    "revision": "0e89246cb86e030c92e16e5a5be27ea3",
+    "url": "/static/media/Business-Loans.0e89246c.jpg"
+  },
+  {
+    "revision": "13fc95d9d6e14dd66f300160549d9e94",
+    "url": "/static/media/CVV-Online-Order.13fc95d9.jpg"
+  },
+  {
+    "revision": "08d8d864be1894b0dbe7fa51e84c08f8",
+    "url": "/static/media/Car-Rental-GPS-2.08d8d864.jpg"
+  },
+  {
+    "revision": "a346356a6904f2778cb714e368bbb293",
+    "url": "/static/media/Cash-advance-form-top-banner.a346356a.jpg"
+  },
+  {
+    "revision": "19548323de0b7db0fcdcfcd5e52767e1",
+    "url": "/static/media/Close-Icon.19548323.svg"
+  },
+  {
+    "revision": "862ed4406e11bfbda03c2b5df5cb1f18",
+    "url": "/static/media/Clover-Family.862ed440.png"
+  },
+  {
+    "revision": "fded41790dadfa9357c9a1ba65afa100",
+    "url": "/static/media/Clover-Flex-POS-2.fded4179.jpg"
+  },
+  {
+    "revision": "4bbb7e621ced2ec9d3c4dcf28ed5345b",
+    "url": "/static/media/Days-of-the-week-batch-funding-SR17-8R2.4bbb7e62.png"
+  },
+  {
+    "revision": "669505f5c5a5fb2a4c234bacfa59a86c",
+    "url": "/static/media/Dejavoo-Z8-768x728-1.669505f5.jpg"
+  },
+  {
+    "revision": "2d70e65ee208cf38b7428d0caab3760a",
+    "url": "/static/media/Devices.2d70e65e.png"
+  },
+  {
+    "revision": "767bd165a8f52f072cbf72ce1c60cb1b",
+    "url": "/static/media/Discounts-For-Large-Purchases.767bd165.jpg"
+  },
+  {
+    "revision": "a2b5c28ac0b5d7c2d43bb11d24396861",
+    "url": "/static/media/EBT-03.a2b5c28a.png"
+  },
+  {
+    "revision": "2445119516ae156a352eb38c98c3ba22",
+    "url": "/static/media/Email-Icon.24451195.svg"
+  },
+  {
+    "revision": "305e8dd9ddb468ba938ee7aeae57303e",
+    "url": "/static/media/Facebook-Icon.305e8dd9.svg"
+  },
+  {
+    "revision": "c68431572b8a4592b4e6e5bbb94303ec",
+    "url": "/static/media/Gift-Cards-Loyalty-Program.c6843157.jpg"
+  },
+  {
+    "revision": "147870ebb6c2fa8bebdfe027c65c6cb4",
+    "url": "/static/media/HMS_Locations_map_SR17-5R2.147870eb.jpg"
+  },
+  {
+    "revision": "18cdde10a68616a7a6d8d63e62f26de6",
+    "url": "/static/media/Happy-Casheir-Using-Clover-POS.18cdde10.jpg"
+  },
+  {
+    "revision": "2b9db8d8653f2e650d7c01d96c06fe0e",
+    "url": "/static/media/Happy-Customer-.2b9db8d8.jpeg"
+  },
+  {
+    "revision": "f08192bea770608311266898580c59a6",
+    "url": "/static/media/Happy-Person.f08192be.jpg"
+  },
+  {
+    "revision": "374572e321205af8c4899aaad03c03ee",
+    "url": "/static/media/Host-Merchant-Services-Review.374572e3.jpg"
+  },
+  {
+    "revision": "1691f1b7d6f4c2cca32176e4dd1ccd4b",
+    "url": "/static/media/How-To-Accept-EBT-1.1691f1b7.jpg"
+  },
+  {
+    "revision": "35a0a1be663fad8c8cda75d7e51b341b",
+    "url": "/static/media/IWL-250.35a0a1be.jpg"
+  },
+  {
+    "revision": "46788ef96cff248026d62a4e51aaeb01",
+    "url": "/static/media/Independent-Sales-Image.46788ef9.png"
+  },
+  {
+    "revision": "297e54217c8e0e6753c9ef41e1a0c64d",
+    "url": "/static/media/Ingenico-iCT220-Image-285x300-1.297e5421.png"
+  },
+  {
+    "revision": "7cb06df33c4893d274c2bfc2c38cfbf4",
+    "url": "/static/media/Interchange-Plus-VS-Flate-Rate-Infographic_SR18-3R3.7cb06df3.jpg"
+  },
+  {
+    "revision": "be3d3438b5d5abe80b76a6b5e5ab5a42",
+    "url": "/static/media/Like Snow.be3d3438.ttf"
+  },
+  {
+    "revision": "55582991c3ad07c1befd47a8525fe975",
+    "url": "/static/media/Linkedin-Icon.55582991.svg"
+  },
+  {
+    "revision": "55fb76b4dd58fba8d2c8ce160e23b84b",
+    "url": "/static/media/Lou-2-hms.55fb76b4.png"
+  },
+  {
+    "revision": "1fa158f42801d2934f43df6b2a5d3df7",
+    "url": "/static/media/Loyalty-Program.1fa158f4.jpg"
+  },
+  {
+    "revision": "fa9363bd314f544ffd521b89d264dd2e",
+    "url": "/static/media/Menu-Icon.fa9363bd.svg"
+  },
+  {
+    "revision": "61f3bc709f3ae1539ee0a81762f41eca",
+    "url": "/static/media/Mobile-processing-banner-4.61f3bc70.jpg"
+  },
+  {
+    "revision": "12f600dac54f240a739c52e3b9fa17e6",
+    "url": "/static/media/Money-Background.12f600da.jpg"
+  },
+  {
+    "revision": "da1e23773a7835b72b302d58f0931542",
+    "url": "/static/media/NFT.da1e2377.gif"
+  },
+  {
+    "revision": "dde964ec8e6cfbff795e11782762c77c",
+    "url": "/static/media/POS-Bonsai-Sales-Tools-Graphic-SR17-8R2.dde964ec.png"
+  },
+  {
+    "revision": "6d679f5908ff1fc0721f06de14a11c79",
+    "url": "/static/media/Partner-Portal-graphic-Accurate-Residuals-SR19-4R1-panorama.6d679f59.jpg"
+  },
+  {
+    "revision": "56d03a17860eab00f1419b557dea7224",
+    "url": "/static/media/Partner-Portal-graphic-Get-paid-on-time-SR19-4R1-panorama.56d03a17.jpg"
+  },
+  {
+    "revision": "0336ebc26ce0080811cd0ec47875a123",
+    "url": "/static/media/Partner-Portal-graphic-SR19-4R1-panorama.0336ebc2.jpg"
+  },
+  {
+    "revision": "45410e3fdccd5c8f60426da8316f6469",
+    "url": "/static/media/Partner-Portal-graphic-dedicated-account-manager-SR19-4R1-panorama.45410e3f.jpg"
+  },
+  {
+    "revision": "33836a218426cf74fd3524e75e80b23f",
+    "url": "/static/media/Paying-With-Cash-Discount.33836a21.jpg"
+  },
+  {
+    "revision": "b9ce4ff4e98d1811519ffe6d0e1ee6ab",
+    "url": "/static/media/Payment-Processing-For-Merchants.b9ce4ff4.jpg"
+  },
+  {
+    "revision": "2b4849d6c3770aa72a56cea3aa76341b",
+    "url": "/static/media/Person-Calling.2b4849d6.jpg"
+  },
+  {
+    "revision": "9a6f4d341e7cb1c2c822f43f12ab2fbc",
+    "url": "/static/media/Phone-Icon.9a6f4d34.svg"
+  },
+  {
+    "revision": "13b21bf61dd22f693210e38be7f12580",
+    "url": "/static/media/Point-of-sale-systems-1-panorama.13b21bf6.jpg"
+  },
+  {
+    "revision": "bdfb2fa3b66c8c0e2d3eba5554ad1ff7",
+    "url": "/static/media/Protection-Against-A-Data-Breach.bdfb2fa3.jpg"
+  },
+  {
+    "revision": "f08a93d25d314bc1ef8fd35b5b08a21e",
+    "url": "/static/media/Provide-Cash-Discounts-To-Customers.f08a93d2.jpg"
+  },
+  {
+    "revision": "52c9d2fdcde0e476e4db3cc2e01c1225",
+    "url": "/static/media/Rambler-3.0_product-image.52c9d2fd.png"
+  },
+  {
+    "revision": "297dbef7b4ebfa1f915b3173b0879fa6",
+    "url": "/static/media/Red-Left-Arrow-Icon.297dbef7.svg"
+  },
+  {
+    "revision": "4715a3b13d23dd0841f206fb5b7ab392",
+    "url": "/static/media/Restaurant-Block-Lightened-25percent.4715a3b1.jpg"
+  },
+  {
+    "revision": "fff18046313c86445070cc4e36818b24",
+    "url": "/static/media/SNAP-logo-for-EBT.fff18046.jpeg"
+  },
+  {
+    "revision": "b4a50934b3436d4b570983670fb5fa0a",
+    "url": "/static/media/Screen-Shot-2019-06-19-at-2.32.04-PM.b4a50934.png"
+  },
+  {
+    "revision": "40d580f097c6c72babb1bde5ba27ed58",
+    "url": "/static/media/Small-Business-Cash-Advance.40d580f0.jpg"
+  },
+  {
+    "revision": "27d371b17565ab29e50e038d2b5507e9",
+    "url": "/static/media/Small-Ticket-Pricing.27d371b1.jpg"
+  },
+  {
+    "revision": "2ed98ce2c78ec06846de716392aa2e81",
+    "url": "/static/media/Smiling-Business-Owner.2ed98ce2.jpg"
+  },
+  {
+    "revision": "6534b856fc51eb8db5a05a4273cb25ec",
+    "url": "/static/media/Twitter-Icon.6534b856.svg"
+  },
+  {
+    "revision": "e1091c6db8cff5b8ae16c9051dc8049d",
+    "url": "/static/media/USA-time-zone-map-Batch-Times.e1091c6d.jpg"
+  },
+  {
+    "revision": "63c02ae4f9df39146b2d3d870a10ba01",
+    "url": "/static/media/VX520_1_med.63c02ae4.jpg"
+  },
+  {
+    "revision": "ad1e52bf4012bc3df554c642c1f1fa6c",
+    "url": "/static/media/Verifone-vx805.ad1e52bf.jpg"
+  },
+  {
+    "revision": "8924ce99f1050f2bd9dd184740b2bf77",
+    "url": "/static/media/Why-Choose-HMS-Banner-2.8924ce99.jpg"
+  },
+  {
+    "revision": "4802377438058f03b6943c58639d0ec7",
+    "url": "/static/media/a920_-_1_1024x1024.48023774.png"
+  },
+  {
+    "revision": "be00d08ea621631e38a14911b575c118",
+    "url": "/static/media/background.be00d08e.png"
+  },
+  {
+    "revision": "ce1a70a5a5a2056cc912903569ce3b6f",
+    "url": "/static/media/bank-note-panorama.ce1a70a5.jpg"
+  },
+  {
+    "revision": "956412dfa8369433619031da971dcee7",
+    "url": "/static/media/banner.956412df.png"
+  },
+  {
+    "revision": "6ec7dfdb92568a9df0862b4fc9b8f7d7",
+    "url": "/static/media/clover-pos.6ec7dfdb.png"
+  },
+  {
+    "revision": "da277953e6f9e8d1e37066901b551904",
+    "url": "/static/media/contactless-card-payment-74484201-1.da277953.jpg"
+  },
+  {
+    "revision": "ee5ff3e7566b46b02beaed5896dbbc2a",
+    "url": "/static/media/contactless-card-payment-74484201-825x340-1.ee5ff3e7.jpg"
+  },
+  {
+    "revision": "31d1497f6937635f80e8b186a56704a1",
+    "url": "/static/media/electronic-payment-concept-18521448-1.31d1497f.jpg"
+  },
+  {
+    "revision": "b7fa35a6ec35f240583a59d8f8040164",
+    "url": "/static/media/hand-with-credit-card-above-laptop-keyboard-179016423-1.b7fa35a6.jpg"
+  },
+  {
+    "revision": "e7e578f38e73bd23b23fae11043022f3",
+    "url": "/static/media/hms-laptopandcard.e7e578f3.jpg"
+  },
+  {
+    "revision": "5c3e34b37d9d721ccd5176d7e764d717",
+    "url": "/static/media/iCT250.5c3e34b3.jpg"
+  },
+  {
+    "revision": "e091a5960706ad815af1308dfebc5bf9",
+    "url": "/static/media/ingenico_ipp320.e091a596.jpg"
+  },
+  {
+    "revision": "5833002bb26a2e228753463d5af64aee",
+    "url": "/static/media/ipp310.5833002b.jpg"
+  },
+  {
+    "revision": "5615bb777bbdb4edc07fa7ce075b63b1",
+    "url": "/static/media/logo.5615bb77.png"
+  },
+  {
+    "revision": "93d207f275c761634ce171ffc98cd0e1",
+    "url": "/static/media/mobileHomeHeroSwift.93d207f2.png"
+  },
+  {
+    "revision": "848e27aa2cd6132f94a0ec28a9d75278",
+    "url": "/static/media/mpop-1.848e27aa.png"
+  },
+  {
+    "revision": "251ab6b6f939c383bf7a3045d2bde641",
+    "url": "/static/media/panorama.251ab6b6.jpg"
+  },
+  {
+    "revision": "ea4a412c2e0b6b5e552155e34d887a63",
+    "url": "/static/media/paying-by-credit-card-AEMKGRF.ea4a412c.jpg"
+  },
+  {
+    "revision": "a02d6fad8d291e5f2787873e123714df",
+    "url": "/static/media/paying-for-organic-food-with-credit-card-EEMXDU5 (1).a02d6fad.jpg"
+  },
+  {
+    "revision": "a02d6fad8d291e5f2787873e123714df",
+    "url": "/static/media/paying-for-organic-food-with-credit-card-EEMXDU5.a02d6fad.jpg"
+  },
+  {
+    "revision": "c9777c24d82426379107e836e5341d7f",
+    "url": "/static/media/pretty-customer-making-a-mobile-payment-56793048-1.c9777c24.jpg"
+  },
+  {
+    "revision": "a10eb7c78cb01684050c1d4131770662",
+    "url": "/static/media/student-849825_640-1.a10eb7c7.jpg"
+  },
+  {
+    "revision": "2278ea05dcd4becbc510adfe72606b1f",
+    "url": "/static/media/swipesimpleimage.2278ea05.png"
+  },
+  {
+    "revision": "428bfeab6abcf26d4e152c99901027ad",
+    "url": "/static/media/tablet_phone_processing.428bfeab.png"
+  },
+  {
+    "revision": "f0f42eff746dddf5278f2577a97f1671",
+    "url": "/static/media/the-man-processing-credit-card-transaction-with-pos-terminal-121237867.f0f42eff.jpg"
+  },
+  {
+    "revision": "373d7eb6167ea573df1f1c0e04162f82",
+    "url": "/static/media/vexxhost-feature.373d7eb6.png"
+  },
+  {
+    "revision": "2739bdeab8efd44d2a5321768b0a1084",
+    "url": "/static/media/vital-mobile-main.2739bdea.jpg"
+  },
+  {
+    "revision": "8e48401a00be2a5fa4bbca110b42087c",
+    "url": "/static/media/vital-select-SR19-5R1.8e48401a.jpg"
+  },
+  {
+    "revision": "17529df38e3ccd3de060baea59ebfbe4",
+    "url": "/static/media/vital-select-x8.17529df3.jpg"
+  },
+  {
+    "revision": "3f69aad6a596b75973e28404286df75b",
+    "url": "/static/media/website_banner_Vital_POS_options.3f69aad6.jpg"
+  },
+  {
+    "revision": "24f8cbc014acefa2cb6bee8a93299ac3",
+    "url": "/static/media/website_banner_agent_program-1.24f8cbc0.jpg"
+  },
+  {
+    "revision": "b96a1b198bc67a7afd55766b996716e6",
+    "url": "/static/media/website_banner_ecommerce_trends_1.b96a1b19.jpg"
+  },
+  {
+    "revision": "b5b53002b356e3f2987ca13e40a08901",
+    "url": "/static/media/website_banner_next_day_funding-1.b5b53002.jpg"
+  },
+  {
+    "revision": "2be09fb87946e7eec35a369a3cb325a1",
+    "url": "/static/media/xcart-logo.2be09fb8.jpg"
+  },
+  {
+    "revision": "8816fa5d99d375a575ccfd2c54c64234",
+    "url": "/static/media/young-woman-pay-by-mobile-phone-in-cafe-LY5HM64.8816fa5d.jpg"
+  },
+  {
+    "revision": "7ce505356e0789aa64d51503492afe66",
+    "url": "/static/media/young-woman-paying-with-credit-card-in-cafe-H4U3T25.7ce50535.jpg"
+  }
+]);
